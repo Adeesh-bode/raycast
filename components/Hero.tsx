75c711ebbf46b96be399fc73386d7d2e97fb441c
adesh.exc <bodead@rknec.edu>
2024-07-23 15:39:32 +0000

    navbar done

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col justify-'>
+        <h1 className='text-3xl' >Your shortcut to everything.</h1>
+        <h2 className='text-xl' >
+            A collection of powerful productivity tools all within an extendable launcher. Fast, ergonomic and reliable.
+        </h2>
+    </div>
+  )
+}
+
+export default Hero
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,29 +5,31 @@ import { NavLinks } from "@/constants";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-20 w-auto  h-20 bg-gray-700 flex mx-16 ">
+    <div className="fixed top-6  w-full  h-16  flex-center">
+        <div className="bg-[#0e0f10] flex justify-between items-center gap-40 px-7 py-5 mx-12 rounded-xl border border-y-2 border-neutral-900  border-l-1 border-r-1 ">
         <div>
             <span></span>
             <span>Raycast</span>
         </div>
 
-        <nav>
+        <nav className="flex justify-between gap-8  ">
             {
                 NavLinks.map(({ label, route }) => (
-                    <Link key={label} href={route}>
+                    <Link key={label} href={route} className="text-gray-400 hover:text-white text-sm font-semibold ">
                         {label}
                     </Link>
                 ))
             }
         </nav>
 
-        <div>
-            <span>Sign In</span>
-            <button>
-                <FaApple />
-                <span> Download </span>
+        <div className="h-fit flex justify-between items-center gap-7 ">
+            <span className="text-gray-400 text-sm font-semibold hover:text-white">Log in</span>
+            <button className="bg-white rounded-md flex justify-between items-center gap-2 py-1 px-2 ">
+                <FaApple  color="black" className="flex-center" />
+                <span className="text-black"> Download </span>
             </button>
         </div>
+        </div>    
     </div>
   )
 }
