08fe664c4ae4aeb0d9e8e995587655019f644af3
Adesh Bode <bodead@rknec.edu>
2024-07-25 02:36:25 -0700

    switch button component

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -9,11 +9,11 @@ interface SwitchProps {
 }
 
 export const iconMap = {
-    clipboard: <FaRegClipboard />,
-    ai: <BsStars />,
-    emoji: <FaRegFaceSmile />,
-    calculator: <BsCalculator />,
-    management: <CgMenuGridO />,
+    clipboard: <FaRegClipboard size={30} />,
+    ai: <BsStars size={30} />,
+    emoji: <FaRegFaceSmile size={30} />,
+    calculator: <BsCalculator size={30} />,
+    management: <CgMenuGridO size={30} />,
 };
 
 const Switch: React.FC<SwitchProps> = ({ selectWindow }) => { // or can have assign inline type 
@@ -22,12 +22,12 @@ const Switch: React.FC<SwitchProps> = ({ selectWindow }) => { // or can have ass
     };
 
     return (
-        <div className="w-full h-20 flex-center">
-            <div className="w-1/5 h-12 flex-center gap-2 rounded-md border border-neutral-600 overflow-y-visible">
+        <div className="w-full h-24 mb-8 flex-center">
+            <div className="w-1/4 h-20 flex-center gap-2 py-3 px-2 rounded-3xl bg-neutral-900/70 border border-neutral-600 overflow-y-visible">
                 {Object.entries(iconMap).map(([key, Icon]) => (
                     <div
                         key={key}
-                        className="flex-center border p-2 border-neutral-600 rounded-md bg-gray-500/35 hover:scale-110"
+                        className="flex-center  border p-3 px-4 border-neutral-600 rounded-lg bg-gray-500/35 hover:scale-110"
                         onClick={() => handleChangeWindow(key)}
                     >
                         {Icon}
