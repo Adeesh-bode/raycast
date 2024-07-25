99d1f86da8e9752dbaee6a8035407e0251a5afee
Adesh Bode <bodead@rknec.edu>
2024-07-25 07:07:03 -0700

    change in screen section

--- components/ScreenDemo.tsx
+++ components/ScreenDemo.tsx
@@ -2,7 +2,7 @@ import Screen from "./Screen";
 
 const ScreenDemo = () => {
   return (
-    <div className="w-full min-h-screen  flex flex-col justify-start items-center ">
+    <div className="w-full min-h-screen  flex flex-col justify-start items-center text-xl ">
         <div className="flex flex-col justify-between items-center gap-1">
             <h2 className="text-white text-center ">Take shortcuts, not detours.</h2>
             <h3 className="text-neutral-500 text-center">One interface, everything you need.</h3>
@@ -15,7 +15,7 @@ const ScreenDemo = () => {
 
         {/* dynamic content based on above selectn in bar */}
          
-        <div className=" w-1/2 text-neutral-600 text-center">
+        <div className=" w-1/2 text-neutral-600 text-center text-xl ">
             <span className="text-white text-center " >Remember Everything.</span> 
             Stop playing Clipboard ping pong - with Clipboard History you&apos;ll never forget anything
         </div>
